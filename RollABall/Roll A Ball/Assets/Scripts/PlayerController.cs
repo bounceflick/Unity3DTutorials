44de1709d600305d3633bec7fa5e4cd0bcacf7ff
First Lasst <First Lasst>
2016-06-24 14:17:12 -0700

    Added ability to remove objects when triggers (had to make objects Trigger, with Rigidbody, and kinematic

--- RollABall/Roll A Ball/Assets/Scripts/PlayerController.cs
+++ RollABall/Roll A Ball/Assets/Scripts/PlayerController.cs
@@ -17,8 +17,17 @@ public class PlayerController : MonoBehaviour {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveHorizontal);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Destroy(other.gameObject);
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false); 
+        }
     }
 }
